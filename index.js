ca07b41c19502ed2e56918010afb7a00dc89e161
Aye-Chan009 <ayechan.spmsc@gmail.com>
2024-08-04 07:32:22 +0800

    adding files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+//API for web visit counter
+const counter = document.querySelector(".count");
+async function updateCounter() {
+    //let response = await fetch("https://zqini5jtlkzelhgdzsudbq47ky0fnxzb.lambda-url.ap-southeast-2.on.aws/");
+    //let response = await fetch("https://api.aye-chan.net/");
+    let data = await response.json();
+    counter.innerHTML = `This webpage has been visited ${data} times`;
+}
+updateCounter();
+
+//get current date
+const currentdate = document.querySelector(".currentdate");
+async function updateCurrentDate() {
+    const now = new Date();
+    const current = now.toLocaleString();
+    currentdate.innerHTML = `Last visited on: ${current}`;
+}
+updateCurrentDate();
+
+const btnScrollToTop = document.querySelector(".btnScrollToTop");
+
+// scroll to top of page when button clicked
+btnScrollToTop.addEventListener("click", e => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth"
+  });
+});
+
+// toggle 'scroll to top' based on scroll position
+window.addEventListener('scroll', e => {
+  btnScrollToTop.style.display = window.scrollY > 400 ? 'block' : 'none';
+});
