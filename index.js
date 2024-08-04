f29af7051b65bbce1fb9a7086dfb33fb64a549fd
Aye-Chan009 <ayechan.spmsc@gmail.com>
2024-08-05 04:35:12 +0800

    restoreoriginal

--- index.js
+++ index.js
@@ -32,32 +32,3 @@ btnScrollToTop.addEventListener("click", e => {
 window.addEventListener('scroll', e => {
   btnScrollToTop.style.display = window.scrollY > 400 ? 'block' : 'none';
 });
-
-
-//send email
-function invokeAWSAPI(e) {
-  e.preventDefault();           
-  var name = $("#name").val();
-  var email = $("#email").val();
-  var msg = $("#message").val();
-  var data = {
-     name : name,
-     email : email,
-     msg : msg
-   };
-$.ajax({
-    type: "POST",
-    url : "https://equskrveuqo7gdpvvz4dkld52u0oqqod.lambda-url.ap-southeast-2.on.aws/",
-    dataType: "json",
-    crossDomain: "true",
-    contentType: "application/json; charset=utf-8",
-    data: JSON.stringify(data),
-    success: function () {
-      alert("Successful");
-      document.getElementById("contact-form").reset();
-  location.reload();
-    },
-    error: function () {
-      alert("unsuccessful");
-    }});
-}
